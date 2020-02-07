398deda8d70676f5dfa54424465cc93ab3a96814
Xandy <xandy_yt@hotmail.com>
2020-02-07 16:44:51 -0300

    Created second page

---
+++ js/option-rock.js
@@ -0,0 +1,9 @@
+function muda() {
+  let img = document.getElementsByClassName("social");
+  img.src = "../img/Group.png";
+  console.log(img);
+}
+
+function f() {
+  document.getElementById("svs")
+}
